1cf5313318c69fbf11d9893ebf92572ec92ea828
Sahil.Agarwal <sahil.agarwal@SahilAgarwals-MacBook-Pro.local>
2025-05-07 13:30:38 +0530

    custom response based on PATH

--- app/main.go
+++ app/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -25,38 +27,32 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			continue
 		}
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 		// Handle the connection in a separate function
-		// handleConnection(conn)
+		handleConnection(conn)
 	}
 }
 
-// func handleConnection(conn net.Conn) {
-// 	defer conn.Close()
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
 
-// 	// Read the request
-// 	reader := bufio.NewReader(conn)
-// 	requestLine, err := reader.ReadString('\n')
-// 	if err != nil {
-// 		fmt.Println("Error reading request: ", err.Error())
-// 		return
-// 	}
+	// Read the request
+	reader := bufio.NewReader(conn)
+	requestLine, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading request: ", err.Error())
+		return
+	}
 
-// 	// // Parse the request line
-// 	// requestLine = strings.TrimSpace(requestLine)
-// 	// if strings.HasPrefix(requestLine, "GET ") {
-// 	// 	// Write a simple HTTP response
-// 	// 	response := "HTTP/1.1 200 OK\r\n" +
-// 	// 		"Content-Type: text/plain\r\n" +
-// 	// 		"Content-Length: 13\r\n" +
-// 	// 		"\r\n" +
-// 	// 		"Hello, world!"
-// 	// 	conn.Write([]byte(response))
-// 	// } else {
-// 	// 	// Respond with 400 Bad Request for unsupported methods
-// 	// 	response := "HTTP/1.1 400 Bad Request\r\n" +
-// 	// 		"Content-Length: 0\r\n" +
-// 	// 		"\r\n"
-// 	// 	conn.Write([]byte(response))
-// 	// }
-// }
+	// Parse the request line
+	requestLine = strings.TrimSpace(requestLine)
+	if strings.HasPrefix(requestLine, "GET ") {
+		splitRequest := strings.Split(requestLine, " ")
+		if splitRequest[1] == "/" {
+			response := "HTTP/1.1 200 OK\r\n\r\n"
+			conn.Write([]byte(response))
+		} else {
+			response := "HTTP/1.1 404 Not Found\r\n\r\n"
+			conn.Write([]byte(response))
+		}
+	}
+}
